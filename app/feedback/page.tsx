975b98e742aff187af3f8943b4d5e8810aaa03a8
codymaruf <azharmaruf900@gmail.com>
2025-08-01 21:04:10 +0000

    v0.1.4

--- app/feedback/page.tsx
+++ app/feedback/page.tsx
@@ -1,5 +1,10 @@
 import Feedback from "@/app/section/Feedback";
 
 export default function FeedbackPage() {
-  return <Feedback />;
+  const dummyAnswers = {
+    question1: "My answer to question 1",
+    question2: "Answer for question 2",
+  };
+
+  return <Feedback userAnswers={dummyAnswers} />;
 }
